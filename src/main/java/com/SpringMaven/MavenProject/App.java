d715683a86dbf713f74c86006b137d4dc1b7739e
balajirakh <149042936+balajirakh@users.noreply.github.com>
2025-01-21 11:13:44 +0530

    commit for hello neha

--- src/main/java/com/SpringMaven/MavenProject/App.java
+++ src/main/java/com/SpringMaven/MavenProject/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hello");
         System.out.println("hello bala");
+         System.out.println("hello neha");
     }
 }
